dfab561e6555e21c3e19b4a1b1cd42978fbc7280
Adam Bridgens <ajb@bridgens.dk>
2024-04-30 10:30:08 +0200

    namef ix

--- Server/Server.API/DTO/FriendDTO.cs
+++ Server/Server.API/DTO/FriendDTO.cs
@@ -4,16 +4,16 @@ namespace Server.API.DTO
 {
     public class FriendDTO
     {
-        public string? FriendId { get; set; }
+        public string? Name { get; set; }
         public DateTime FriendsSince { get; set; }
 
         public bool IsAccepted { get; set; }
         public static FriendDTO FormFriendship(string userName, Friendship f)
         {
-            var friendId = f.User1.UserName == userName ? f.User2.UserName : f.User1.UserName;
+            var friendName = f.User1.UserName == userName ? f.User2.UserName : f.User1.UserName;
             FriendDTO friendDTO = new FriendDTO
             {
-                FriendId = friendId,
+                Name = friendName,
                 FriendsSince = f.date,
                 IsAccepted = f.Status == "Accepted"
             };
