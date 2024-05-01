9deccd3f99efd4409401727db14e57f355c5c750
Jonas <jona813k@hotmail.com>
2024-05-01 13:30:26 +0200

    twicking and organizing

--- Client/Client.UI/ViewModels/SettingsViewModel.cs
+++ Client/Client.UI/ViewModels/SettingsViewModel.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Client.Library.Interfaces;
-using Client.Library.Models;
+﻿using Client.Library.Interfaces;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Client.Library.Services.Interfaces;
 using CommunityToolkit.Mvvm.Input;
-using Newtonsoft.Json;
-using System.Collections.ObjectModel;
-using Client.Library.Services;
+
 
 namespace Client.UI.ViewModels
 {
     public partial class SettingsViewModel : ObservableObject
     {
 
-        private readonly IApiService _apiService;
         private readonly INavigationService _navigationService;
         public SettingsViewModel(IApiService apiService, INavigationService navigationService)
         {
-            _apiService = apiService;
             _navigationService = navigationService;
         }
 
