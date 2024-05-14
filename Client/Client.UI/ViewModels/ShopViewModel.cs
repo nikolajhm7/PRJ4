5ef69eb61c110be8587f64ab7576aa19c4542fe0
JonasGjorupEriksen <47977978+JonasGjorupEriksen@users.noreply.github.com>
2024-05-14 10:17:07 +0200

    its a start

---
+++ Client/Client.UI/ViewModels/ShopViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Client.Library.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Client.Library.Services.Interfaces;
+using CommunityToolkit.Mvvm.Input;
+
+
+namespace Client.UI.ViewModels
+{
+    public partial class ShopViewModel : ObservableObject
+    {
+
+        private readonly INavigationService _navigationService;
+        public ShopViewModel(IApiService apiService, INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        [RelayCommand]
+        public async Task GoBack()
+        {
+            await _navigationService.NavigateBack();
+        }
+
+    }
+}
---
+++ Client/Client.UI/Views/ShopPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Client.UI.Views;
+using Microsoft.Maui.Controls;
+using Client.UI.ViewModels;
+
+
+public partial class ShopPage : ContentPage
+{
+    public ShopPage(ShopViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = vm;
+    }
+}
